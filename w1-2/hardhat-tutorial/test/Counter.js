65b8d65d1b1d8b6cd0326b8e58b776157c6b77c6
何琦 <>
2023-03-13 21:39:32 +0800

    w1-2 测试用例

---
+++ w1-2/hardhat-tutorial/test/Counter.js
@@ -0,0 +1,28 @@
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { expect } = require("chai");
+
+describe("Counter contract", function () {
+  // 您可以通过使用固定装置来避免代码重复并提高测试套件的性能。 
+  // fixture 是一个设置函数，仅在第一次调用时运行。
+  async function deployCounterFixture() {
+    const [owner, addr1] = await ethers.getSigners();
+
+    const Counter = await ethers.getContractFactory("Counter");
+
+    const hardhatCounter = await Counter.deploy();
+
+    return { Counter, hardhatCounter, owner, addr1 };
+  }
+
+  it("Only owner can call add() function", async function () {
+    const { hardhatCounter, addr1 } = await loadFixture(deployCounterFixture);
+    await hardhatCounter.add(1);
+    expect(await hardhatCounter.counter()).to.equal(1);
+
+    try {
+      await hardhatCounter.connect(addr1).add(1);
+    } catch (error) {
+      expect(await hardhatCounter.counter()).to.equal(1);
+    }
+  });
+});
