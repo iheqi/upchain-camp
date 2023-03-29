432250e023f1558bacd0a80fe12a4d640af27841
何琦 <>
2023-03-29 22:35:56 +0800

    使用 @wagmi/cli 生成包装函数

--- w4-1/wagmi-next-rainbowkit/src/components/Vault.tsx
+++ w4-1/wagmi-next-rainbowkit/src/components/Vault.tsx
@@ -4,34 +4,23 @@ import { useAccount, useContractRead, useContractWrite, usePrepareContractWrite 
 import erc20GaGaAbi from '../abi/GaGa.json'
 import vaultAbi from '../abi/Vault.json'
 
-
 const gagaAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 const vaultAddress = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512';
 
 export function Vault() {
   const { address, isConnected } = useAccount()
 
-
-  
-
-
-  // console.log(address, data, ethers.utils.formatUnits(data as BigNumber, 18))
-
-
-
   return (
     <>
       <h2>Vault</h2>
-
       <GaGaBalance address={address}/>
       <VaultBalance address={address}/>
     </>
   )
 }
 
-
 function GaGaBalance({ address }: any) {
-  const { data: balance, isError, refetch: refetchBalance } = useContractRead({
+  const { data: balance, refetch: refetchBalance } = useContractRead({
     address: gagaAddress,
     abi: erc20GaGaAbi,
     functionName: 'balanceOf',
@@ -52,7 +41,6 @@ function GaGaBalance({ address }: any) {
     // refetchBalance()
   }, [])
 
-
   return (
     <p>
       <span>您的 GaGa 币余额：{ethers.utils.formatUnits(balance as BigNumber, 0)}</span> 
@@ -68,6 +56,15 @@ function VaultBalance({ address }: any) {
     functionName: 'balanceOf',
     args: [address]
   })
+
+
+  const { config } = usePrepareContractWrite({
+    address: vaultAddress,
+    abi: vaultAbi,
+    functionName: 'mint',
+    args: [10000]
+  })
+
 
   if (isLoading) {
     return (
--- w4-1/wagmi-next-rainbowkit/wagmi.config.ts
+++ w4-1/wagmi-next-rainbowkit/wagmi.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from '@wagmi/cli'
 import { erc20ABI } from 'wagmi'
+import { erc, react } from '@wagmi/cli/plugins'
 
 // https://wagmi.sh/cli/getting-started
 export default defineConfig({
   out: 'src/generated.ts',
-  contracts: [
-    {
-      name: 'erc20',
-      abi: erc20ABI,
-    },
-  ]
+  // contracts: [
+  //   {
+  //     name: 'erc20',
+  //     abi: erc20ABI,
+  //   },
+  // ],
+  plugins: [erc(), react()],
 })
